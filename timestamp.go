af65d9f25f7118f9a195c8e77aec68f1db486252
sidjain28 <siddharth.rjain08@gmail.com>
2015-05-05 16:23:31 +0530

    Create timestamp.go

---
+++ timestamp.go
@@ -0,0 +1,67 @@
+package timestamp
+
+import (
+	"gopkg.in/mgo.v2/bson"
+	"errors"
+	"time"
+)
+
+type Timestamp time.Time
+
+func (t Timestamp) MarshalJSON() ([]byte, error) {
+
+	if y := time.Time(t).Year(); y < 0 || y >= 10000 {
+		// RFC 3339 is clear that years are 4 digits exactly.
+		// See golang.org/issue/4556#c15 for more discussion.
+		return nil, errors.New("Time.MarshalJSON: year outside of range [0,9999]")
+	}
+	return []byte(time.Time(t).Format(`"` + time.RFC3339Nano + `"`)), nil
+
+}
+
+func (t *Timestamp) UnmarshalJSON(b []byte) error {
+	var tim time.Time
+	var err error
+
+	if string(b) != "\"\"" { // check empty string
+		tim, err = time.Parse(`"`+time.RFC3339+`"`, string(b))
+		*t = Timestamp(tim)
+		if err != nil {
+			*t = Timestamp(tim) // return 0 time
+		}
+	}
+
+	return nil
+}
+
+func (t Timestamp) GetBSON() (interface{}, error) {
+	if time.Time(t).IsZero() {
+		return nil, nil
+	}
+
+	return time.Time(t), nil
+}
+
+func (t *Timestamp) SetBSON(raw bson.Raw) error {
+	var tm time.Time
+
+	if err := raw.Unmarshal(&tm); err != nil {
+		return err
+	}
+
+	*t = Timestamp(tm)
+
+	return nil
+}
+
+func (t *Timestamp) String() string {
+	return time.Time(*t).String()
+}
+
+func (t *Timestamp) GetTime() time.Time {
+	return time.Time(*t)
+}
+
+func GetTimeStamp(t time.Time) Timestamp {
+	return Timestamp(t)
+}
